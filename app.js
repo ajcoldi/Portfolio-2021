d836d59368f3daf390d06cee57d0669d01a664ca
ajcoldi <63015086+ajcoldi@users.noreply.github.com>
2021-07-16 12:04:49 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const pageRouter = require('./routes/pageRoutes');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use('/', pageRouter);
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port: ${port}`));
---
+++ routes/pageRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('pages/index')
+})
+router.get('/projects', (req, res) => {
+    res.render('pages/projects')
+})
+router.get('/blogs', (req, res) => {
+    res.render('pages/blogs')
+})
+router.get('/contact', (req, res) => {
+    res.render('pages/contact')
+})
+
+
+
+router.get('/projects/takeaway', (req, res) => {
+    res.render('pages/projects/takeaway/index.ejs')
+})
+
+module.exports = router;
